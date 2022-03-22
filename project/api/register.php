256da2977a6350c6163cfdca137337094dfcdafe
bishalregmi82 <vishalregmi82@gmail.com>
2022-03-22 16:06:55 +0545

    update

--- project/api/register.php
+++ project/api/register.php
@@ -3,11 +3,16 @@ include("connect.php");
 $name =$_POST['name'];
 $password =$_POST['pass'];
 $province =$_POST['province'];
+$addr=$_POST['addr'];
+$gender=$_POST['gender'];
+$role=$_POST['role'];
+$image=$_FILES['photo'];
+$photos=$image['name'];
+$fileerror=$image['error'];
+$filetemp=$image['tmp_name'];
 
 
-
-
-$insert = mysqli_query($connect,"INSERT INTO user(name,pass,province,role,status,votes) VALUES('$name','$password','$province',0,0,0)" );
+$insert = mysqli_query($connect,"INSERT INTO user(name,pass,province,role,status,votes,gender,addr,photo) VALUES('$name','$password','$province','$role',0,0,'$gender','$addr','$filetemp')" );
 if($insert){
     echo` <script>
     alert("sucess");
