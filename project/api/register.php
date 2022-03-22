7097b4694df0b78e4e4bb623b70d9571e9eb4d18
bishalregmi82 <vishalregmi82@gmail.com>
2022-03-22 18:28:07 +0545

    change

--- project/api/register.php
+++ project/api/register.php
@@ -13,7 +13,4 @@ $filetemp=$image['tmp_name'];
 
 
 $insert = mysqli_query($connect,"INSERT INTO user(name,pass,province,role,status,votes,gender,addr,photo) VALUES('$name','$password','$province','$role',0,0,'$gender','$addr','$filetemp')" );
-if($insert){
-    exit();
-    }
-    
+
